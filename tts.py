ec9a8f9b4c2ea9fc62fa74244a468c4ede2769e3
JuanNatalio <jonathanjimenez446138@gmail.com>
2025-03-22 16:20:49 -0400

    Add files via upload

--- tts.py
+++ tts.py
@@ -1,11 +1,12 @@
 import pyttsx3
+import subprocess
+
+subprocess.run(["python3", "gemini.py"], check=True)
 
 # Initialize the converter
 engine = pyttsx3.init()
 
-
-# Open and read the text file
-# The text file will be updated for every response from the therapist
+# Open and read the text fileW
 with open('file.txt', 'r') as file:
     text = file.read()
 
